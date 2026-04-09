5687d491f32b40c101dceca614e0318f41fc7fbb
Hebert <monteiro.hebert@gmail.com>
2026-04-09 05:34:23 -0300

    adicionando screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+  video:false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
